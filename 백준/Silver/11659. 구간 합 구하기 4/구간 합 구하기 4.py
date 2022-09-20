05029ef950a3eb42506935dcb9be7c28d1e65c96
britko <britko@naver.com>
2022-09-20 18:47:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 276 ms, Memory: 40596 KB -Baekj…

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,14 +1,14 @@
 import sys
 
 input = sys.stdin.readline
-_, m = map(int, input().split())
+n, m = map(int, input().split())
 
 a = list(map(int, input().split()))
-sum = [0] * 100001
+sum = []
+sum.append(0)
 
-for i in range(1, len(a) + 1):
-    sum[0] = 0
-    sum[i] = a[i-1] + sum[i-1]
+for i in range(1, n+1):
+    sum.append(a[i-1] + sum[i-1])
 
 for _ in range(m):
     i, j = map(int, input().split())
