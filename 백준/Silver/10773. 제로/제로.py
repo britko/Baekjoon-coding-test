f341631e5f23c94ab0989673588d0f4f945a6f4f
Ko YeongGuk <britko@naver.com>
2022-10-05 16:19:15 +0900

    [Silver IV] Title: 제로, Time: 112 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+k = int(input())
+
+arr = []
+for _ in range(k):
+    n = int(input())
+    if n != 0:
+        arr.append(n)
+    else:
+        arr.pop()
+print(sum(arr))
