2629781e30e10c100c48bdda29753ccc0c65dd25
Ko YeongGuk <britko@naver.com>
2022-12-09 03:02:24 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+q = deque()
+n = int(input())
+
+for _ in range(n):
+    cmd = list(input().split())
+
+    if cmd[0] == 'push_front':
+        q.appendleft(cmd[1])
+    elif cmd[0] == 'push_back':
+        q.append(cmd[1])
+    elif cmd[0] == 'pop_front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif cmd[0] == 'pop_back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.pop())
+    elif cmd[0] == 'size':
+        print(len(q))
+    elif cmd[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif cmd[0] == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
