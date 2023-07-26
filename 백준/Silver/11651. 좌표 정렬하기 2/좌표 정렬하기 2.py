67f125f1a67567ca21fac0211e849d558637382e
Ko YeongGuk <britko@naver.com>
2023-07-26 20:26:58 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 404 ms, Memory: 59204 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+coordinates = [list(map(int, input().split())) for _ in range(n)]
+
+coordinates.sort(key=lambda x : (x[1], x[0]))
+
+for c in coordinates:
+    print(*c)
